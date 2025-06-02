a0f17268ab791ecb9ba15f732315e61852d5f7e5
Daniel Page <dpage105@gmail.com>
2025-06-02 14:42:19 -0500

    Created appsettings.json and added it to .gitignore; Added OpenWeatherAPI class; Added GetUSWeather and GetWorldWeather methods to OpenWeatherAPI class

--- OpenWeatherMap/OpenWeatherMapAPI.cs
+++ OpenWeatherMap/OpenWeatherMapAPI.cs
@@ -1,11 +1,110 @@
+using System.Collections.Immutable;
+using System.Net;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
 namespace OpenWeatherMap;
 
 public class OpenWeatherMapAPI
 {
-    public string ApiKey { get; set; }
-
-    public void GetWeather(HttpClient client, string apiKey)
+    static public void GetUSWeather(HttpClient client, IConfiguration config)
     {
+        string apiKey = config["ApiSettings:MyApiKey"];
+
+        var city = "";
+        var state = "";
+        var country = "us";
+        var weather = "";
+        bool validInput = false;
+
+        do
+        {
+            Console.WriteLine("Please enter the city name: ");
+            city = Console.ReadLine();
+
+            Console.WriteLine("Please enter the two digit state code: ");
+            state = Console.ReadLine();
+
+            var weatherAPI =
+                $"https://api.openweathermap.org/data/2.5/weather?q={city},{state},{country}&appid=0f794640d6d7201d2ac496ca6ff3e06e&units=imperial";
+            var weatherData = client.GetAsync(weatherAPI).Result;
+
+            if (weatherData.IsSuccessStatusCode)
+            {
+                weather = weatherData.Content.ReadAsStringAsync().Result;
+                validInput = true;
+            }
+            else if (weatherData.StatusCode == HttpStatusCode.NotFound)
+            {
+                Console.WriteLine("City with this state code is not valid.  Please try again.\n");
+                validInput = false;
+            }
+        } while (!validInput);
+
+        var json = JObject.Parse(weather);
         
+        var weatherMain = json.SelectToken("weather[0].description").ToString();
+        var temp = json.SelectToken("main.temp").ToString();
+        var feelsLike = json.SelectToken("main.feels_like").ToString();
+        var humidity = json.SelectToken("main.humidity").ToString();
+        var windSpeed = json.SelectToken("wind.speed").ToString();
+        
+        Console.WriteLine($"Weather data for {city}, {state} is as follows:");
+        Console.WriteLine("------------------------------------------------");
+        Console.WriteLine($"Weather: \t{weatherMain}\n");
+        Console.WriteLine($"Temperature: \t{temp}° F");
+        Console.WriteLine($"Feels Like: \t{feelsLike}° F");
+        Console.WriteLine($"Humidity: \t{humidity}%\n");
+        Console.WriteLine($"Wind Speed: \t{windSpeed} mph");
+    }
+    
+    static public void GetWorldWeather(HttpClient client, IConfiguration config)
+    {
+        string apiKey = config["ApiSettings:MyApiKey"];
+
+        var city = "";
+        var country = "";
+        var weather = "";
+        bool validInput = false;
+
+        do
+        {
+            Console.WriteLine("Please enter the city name: ");
+            city = Console.ReadLine();
+
+            Console.WriteLine("Please enter the two digit country code: ");
+            Console.WriteLine("(Country codes may be found at https://countrycode.org/.  Use the two digit ISO code.)");
+            country = Console.ReadLine();
+
+            var weatherAPI =
+                $"https://api.openweathermap.org/data/2.5/weather?q={city},{country}&appid=0f794640d6d7201d2ac496ca6ff3e06e&units=imperial";
+            var weatherData = client.GetAsync(weatherAPI).Result;
+
+            if (weatherData.IsSuccessStatusCode)
+            {
+                weather = weatherData.Content.ReadAsStringAsync().Result;
+                validInput = true;
+            }
+            else if (weatherData.StatusCode == HttpStatusCode.NotFound)
+            {
+                Console.WriteLine("City with this country code is not valid.  Please try again.\n");
+                validInput = false;
+            }
+        } while (!validInput);
+
+        var json = JObject.Parse(weather);
+        
+        var weatherMain = json.SelectToken("weather[0].description").ToString();
+        var temp = json.SelectToken("main.temp").ToString();
+        var feelsLike = json.SelectToken("main.feels_like").ToString();
+        var humidity = json.SelectToken("main.humidity").ToString();
+        var windSpeed = json.SelectToken("wind.speed").ToString();
+        
+        Console.WriteLine($"Weather data for {city}, {country} is as follows:");
+        Console.WriteLine("------------------------------------------------");
+        Console.WriteLine($"Weather: \t{weatherMain}\n");
+        Console.WriteLine($"Temperature: \t{temp}° F");
+        Console.WriteLine($"Feels Like: \t{feelsLike}° F");
+        Console.WriteLine($"Humidity: \t{humidity}%\n");
+        Console.WriteLine($"Wind Speed: \t{windSpeed} mph");
     }
 }
--- OpenWeatherMap/Program.cs
+++ OpenWeatherMap/Program.cs
@@ -1,9 +1,19 @@
 ﻿namespace OpenWeatherMap;
+using System;
+using Microsoft.Extensions.Configuration;
 
 class Program
 {
     static void Main(string[] args)
     {
         var client = new HttpClient();
+
+        var config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .AddEnvironmentVariables()
+            .Build();
+        
+        OpenWeatherMapAPI.GetUSWeather(client, config);
+        OpenWeatherMapAPI.GetWorldWeather(client, config);
     }
 }
