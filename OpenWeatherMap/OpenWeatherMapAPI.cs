455ba56a4fc75ba7aa33ce315a0d49244c76570e
Daniel Page <dpage105@gmail.com>
2025-06-02 17:56:46 -0500

    Created UserAccess class; Added MainMenu and CitySubMenu methods to UserAccess class

--- OpenWeatherMap/OpenWeatherMapAPI.cs
+++ OpenWeatherMap/OpenWeatherMapAPI.cs
@@ -6,16 +6,19 @@ namespace OpenWeatherMap;
 
 public class OpenWeatherMapAPI
 {
-    static public void GetUSWeather(HttpClient client, IConfiguration config)
+    public static void GetUSWeather(HttpClient client, IConfiguration config)
     {
+        //get API key
         string apiKey = config["ApiSettings:MyApiKey"];
 
+        //declare variables
         var city = "";
         var state = "";
         var country = "us";
         var weather = "";
         bool validInput = false;
-
+        
+        //receive and validate user input
         do
         {
             Console.WriteLine("Please enter the city name: ");
@@ -27,42 +30,56 @@ public class OpenWeatherMapAPI
             var weatherAPI =
                 $"https://api.openweathermap.org/data/2.5/weather?q={city},{state},{country}&appid=0f794640d6d7201d2ac496ca6ff3e06e&units=imperial";
             var weatherData = client.GetAsync(weatherAPI).Result;
-
+            
+            //proceed with weather data display if user input is valid
             if (weatherData.IsSuccessStatusCode)
             {
                 weather = weatherData.Content.ReadAsStringAsync().Result;
                 validInput = true;
             }
+            //prompt user to try again if input is invalid
             else if (weatherData.StatusCode == HttpStatusCode.NotFound)
             {
-                Console.WriteLine("City with this state code is not valid.  Please try again.\n");
+                Console.WriteLine("----------------------------------------------------------");
+                Console.WriteLine("City with this state code is not valid.  Please try again.");
+                Console.WriteLine("----------------------------------------------------------\n");
                 validInput = false;
             }
+            //prompt user to try again if something unexpected occurs while processing user input
             else
             {
-                Console.WriteLine($"Request did not process correctly.  Please try again.\n");
+                Console.WriteLine("-----------------------------------------------------");
+                Console.WriteLine("Request did not process correctly.  Please try again.");
+                Console.WriteLine("-----------------------------------------------------\n");
                 validInput = false;
             }
         } while (!validInput);
-
+        
+        //parse user input
         var json = JObject.Parse(weather);
         
+        //stor various data retrieved from weather API in order to display to console
         var weatherMain = json.SelectToken("weather[0].description").ToString();
         var temp = json.SelectToken("main.temp").ToString();
         var feelsLike = json.SelectToken("main.feels_like").ToString();
         var humidity = json.SelectToken("main.humidity").ToString();
         var windSpeed = json.SelectToken("wind.speed").ToString();
         
-        Console.WriteLine($"Weather data for {city}, {state} is as follows:");
+        //display data to console
+        Console.WriteLine($"\nWeather data for {city}, {state} is as follows:");
         Console.WriteLine("------------------------------------------------");
         Console.WriteLine($"Weather: \t{weatherMain}\n");
         Console.WriteLine($"Temperature: \t{temp}° F");
         Console.WriteLine($"Feels Like: \t{feelsLike}° F");
         Console.WriteLine($"Humidity: \t{humidity}%\n");
         Console.WriteLine($"Wind Speed: \t{windSpeed} mph");
+        Console.WriteLine("------------------------------------------------\n");
+        Console.WriteLine($"Please press enter to continue...");
+        Console.ReadLine();
     }
     
-    static public void GetWorldWeather(HttpClient client, IConfiguration config)
+    //see GetUSWeather method for details on each section of code
+    public static void GetWorldWeather(HttpClient client, IConfiguration config)
     {
         string apiKey = config["ApiSettings:MyApiKey"];
 
@@ -91,12 +108,16 @@ public class OpenWeatherMapAPI
             }
             else if (weatherData.StatusCode == HttpStatusCode.NotFound)
             {
-                Console.WriteLine("City with this country code is not valid.  Please try again.\n");
+                Console.WriteLine("------------------------------------------------------------");
+                Console.WriteLine("City with this country code is not valid.  Please try again.");
+                Console.WriteLine("------------------------------------------------------------\n");
                 validInput = false;
             }
             else
             {
-                Console.WriteLine($"Request did not process correctly.  Please try again.\n");
+                Console.WriteLine("-----------------------------------------------------");
+                Console.WriteLine("Request did not process correctly.  Please try again.");
+                Console.WriteLine("-----------------------------------------------------\n");
                 validInput = false;
             }
         } while (!validInput);
@@ -109,16 +130,20 @@ public class OpenWeatherMapAPI
         var humidity = json.SelectToken("main.humidity").ToString();
         var windSpeed = json.SelectToken("wind.speed").ToString();
         
-        Console.WriteLine($"Weather data for {city}, {country} is as follows:");
+        Console.WriteLine($"\nWeather data for {city}, {country} is as follows:");
         Console.WriteLine("------------------------------------------------");
         Console.WriteLine($"Weather: \t{weatherMain}\n");
         Console.WriteLine($"Temperature: \t{temp}° F");
         Console.WriteLine($"Feels Like: \t{feelsLike}° F");
         Console.WriteLine($"Humidity: \t{humidity}%\n");
         Console.WriteLine($"Wind Speed: \t{windSpeed} mph");
+        Console.WriteLine("------------------------------------------------\n");
+        Console.WriteLine($"Please press enter to continue...");
+        Console.ReadLine();
     }
     
-    static public void GetWeatherByCoordinates(HttpClient client, IConfiguration config)
+    //see GetUSWeather method for details on each section of code
+    public static void GetWeatherByCoordinates(HttpClient client, IConfiguration config)
     {
         string apiKey = config["ApiSettings:MyApiKey"];
 
@@ -148,12 +173,16 @@ public class OpenWeatherMapAPI
             }
             else if (weatherData.StatusCode == HttpStatusCode.NotFound)
             {
-                Console.WriteLine("City with this country code is not valid.  Please try again.\n");
+                Console.WriteLine("---------------------------------------------------");
+                Console.WriteLine("These coordinates are not valid.  Please try again.");
+                Console.WriteLine("---------------------------------------------------\n");
                 validInput = false;
             }
             else
             {
-                Console.WriteLine($"Request did not process correctly.  Please try again.\n");
+                Console.WriteLine("-----------------------------------------------------");
+                Console.WriteLine("Request did not process correctly.  Please try again.");
+                Console.WriteLine("-----------------------------------------------------\n");
                 validInput = false;
             }
         } while (!validInput);
@@ -166,13 +195,15 @@ public class OpenWeatherMapAPI
         var humidity = json.SelectToken("main.humidity").ToString();
         var windSpeed = json.SelectToken("wind.speed").ToString();
         
-        Console.WriteLine($"Weather data for latitude {latitude}, longitude {longitude} is as follows:");
+        Console.WriteLine($"\nWeather data for latitude {latitude}, longitude {longitude} is as follows:");
         Console.WriteLine("------------------------------------------------");
-        
         Console.WriteLine($"Weather: \t{weatherMain}\n");
         Console.WriteLine($"Temperature: \t{temp}° F");
         Console.WriteLine($"Feels Like: \t{feelsLike}° F");
         Console.WriteLine($"Humidity: \t{humidity}%\n");
         Console.WriteLine($"Wind Speed: \t{windSpeed} mph");
+        Console.WriteLine("------------------------------------------------\n");
+        Console.WriteLine($"Please press enter to continue...");
+        Console.ReadLine();
     }
 }
